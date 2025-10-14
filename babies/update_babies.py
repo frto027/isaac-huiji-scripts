c6c19ffeca1571d070d6f9632b2bbf6f14ed87b4
frto027 <q602706150@gmail.com>
2025-10-14 14:29:15 +0800

    babies

--- babies/update_babies.py
+++ babies/update_babies.py
@@ -12,9 +12,9 @@ site = isaac.site(__file__)
 tabx = pytabx.HuijiTabx(site.Pages['Data:Babies.tabx'])
 
 import xml.etree.ElementTree as ET
-with (config.game_folder / 'resources-dlc3/babies.xml').open('r') as f:
+with (config.game_folder_resource / 'babies.xml').open('r') as f:
     doc = ET.parse(f).getroot()
-
+tabx.dump("before.json")
 for item in doc:
     assert item.tag == 'baby'
     d = tabx.get_row_by_unique_field("id", int(item.attrib["id"]), create_ok=False)
@@ -27,5 +27,6 @@ for item in doc:
     skinPath = isaac.get_anm2_wiki_path(item.attrib["skin"])
     if skinPath != None:
         d.set("skin", skinPath)
-
+tabx.dump("after.json")
+tabx.save("update repp babies")
 print(tabx.any_changed())
